6caf03e948c108bc857c44a8f6de5887547ff02b
Unpredict2ble <unpredict2ble@gmail.com>
2023-12-30 17:08:07 -0600

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// loads .env file contents into process.env
+dotenv.config();
+
+// connecting to our database
+mongoose
+.connect(process.env.MONGO_URI)
+  .then(() => console.log("[SUCCESS]: Connected to database."))
+  .catch((err) => console.log("[ERROR]: Failed to connect to database.", err))
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server listening on port...3000");
+  console.log("Server listening on port...3000");
 });
