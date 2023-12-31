a112c199e6648922b9912207eb9077101f8c3194
Unpredict2ble <unpredict2ble@gmail.com>
2023-12-30 18:05:41 -0600

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// creating a new mongoose schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// creating model
+const User = mongoose.model("User", userSchema);
+export default User;
